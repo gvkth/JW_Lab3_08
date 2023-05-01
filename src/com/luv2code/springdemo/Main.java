7fa2b52d2549c74519c134d658abdaab32735116
cuongvk <cuongvk.hcm@vnpt.vn>
2023-05-01 16:30:24 +0700

    Finish Lab3.9

--- src/com/luv2code/springdemo/Main.java
+++ src/com/luv2code/springdemo/Main.java
@@ -7,7 +7,13 @@ public class Main {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Coach theCoach = context.getBean("tennisCoach",Coach.class);
+        //call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
+
+        //call method to get the daily fortune
+        System.out.println(theCoach.getDailyFortune());
+
+        //close the context
         context.close();
     }
 }
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,9 +1,17 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component()
 public class TennisCoach implements Coach{
+    private FortuneService fortuneService;
+
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
@@ -12,6 +20,6 @@ public class TennisCoach implements Coach{
     @Override
     public String getDailyFortune() {
         /*TODO: chua cai dat getDailyFortune */
-        return null;
+        return fortuneService.getFortune();
     }
 }
